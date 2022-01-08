3672cdc513bc15f640a28b84790b17142efa019b
suresh <skrao206@gmail.com>
2022-01-08 18:15:41 +0530

    commited this side from ashok

--- test1.py
+++ test1.py
@@ -1,2 +1,8 @@
 x=30
 print(x*2)
+
+print("thid side from ashok")
+print("thid side from ashok")
+print("thid side from ashok")
+print("thid side from ashok")
+print("thid side from ashok")
--- test2.py
+++ test2.py
@@ -1,3 +1,11 @@
 y=40
 
 print("welcome to tst2")
+
+print("this side from ashok222222222")
+print("this side from ashok222222222")
+print("this side from ashok222222222")
+print("this side from ashok222222222")
+print("this side from ashok222222222")
+print("this side from ashok222222222")
+print("this side from ashok222222222")
